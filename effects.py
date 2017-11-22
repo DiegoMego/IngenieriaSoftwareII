e88decce5bb79c21925c4005cbcc5222944d4d1e
Diego Mego <DM2593@gmail.com>
2017-11-22 11:07:44 -0500

    Sorcerer 1st Skill done

--- effects.py
+++ effects.py
@@ -11,21 +11,16 @@ class FlareRed(pg.sprite.Sprite):
         self.groups = game.effect_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.imagemanager = ImageManager.get_instance()
         self.pos = vec(pos)
-        self.image = self.imagemanager.effects["Shoot"][0]
-        self.rect = self.image.get_rect()
         self.hit_rect = copy.copy(EFFECT_RECT)
         self.direction = direction
         self.damage = damage
-        keyhandler = KeyHandler.get_instance()
-        self.x = keyhandler.vel_directions[self.direction][1]
-        self.y = keyhandler.vel_directions[self.direction][2]
-        self.explode = False
+        self.mob_hit = None
+        self.load()
 
     def load(self):
-        self.states = {"Shoot": Shoot(),
-                       "Explode": Explode()}
+        self.states = {"Shoot": Shoot(self),
+                       "Explosion": Explosion(self)}
         self.state_name = "Shoot"
         self.state = self.states[self.state_name]
 
@@ -34,7 +29,7 @@ class FlareRed(pg.sprite.Sprite):
         self.state.done[state_name] = False
         self.state_name = state_name
         self.state = self.states[self.state_name]
-        self.state.start_up()
+        self.state.startup()
 
     def events(self):
         self.state.events()
@@ -44,6 +39,9 @@ class FlareRed(pg.sprite.Sprite):
             if value:
                 self.flip_state(key)
         self.state.update(dt)
+        self.image = self.state.image
+        if not hasattr(self, "rect"):
+            self.rect = self.image.get_rect()
         self.vel = self.state.vel
         self.pos += self.vel
         self.hit_rect.centerx = self.pos.x
@@ -51,24 +49,20 @@ class FlareRed(pg.sprite.Sprite):
         self.collide(collide_effect(self, self.game.mob_sprites))
         self.rect.centerx = self.hit_rect.centerx
         self.rect.centery = self.hit_rect.centery - 30
-        self.action(self.imagemanager.effects["Shoot"])
 
     def collide(self, mob_hit):
         if mob_hit:
-            self.explode = True
-
-    def action(self, action):
-        now = pg.time.get_ticks()
-        if now - self.last_update > 100:
-            self.last_update = now
-            self.current_frame = (self.current_frame + 1) % len(action)
-            self.image = action[self.current_frame]
+            self.mob_hit = mob_hit
 
 class State:
     def __init__(self, effect):
+        self.imagemanager = ImageManager.get_instance()
         self.effect = effect
         self.last_update = 0
         self.current_frame = 0
+
+    def startup(self):
+        pass
 
     def events(self):
         pass
@@ -86,13 +80,20 @@ class State:
 class Shoot(State):
     def __init__(self, effect):
         super().__init__(effect)
-        self.done = {"Explode": False}
+        self.direction = effect.direction
+        keyhandler = KeyHandler.get_instance()
+        self.x = keyhandler.vel_directions[self.direction][1]
+        self.y = keyhandler.vel_directions[self.direction][2]
+        self.done = {"Explosion": False}
         self.clock = pg.time.Clock()
-        self.lifetime = 30
+        self.lifetime = 10
 
     def events(self):
-        if self.effect.explode:
-            self.done["Explode"] = True
+        if self.effect.mob_hit:
+            self.done["Explosion"] = True
+            self.effect.mob_hit.currenthealth -= self.effect.damage
+        if self.lifetime <= 0:
+            self.effect.kill()
 
     def update(self, dt):
         self.clock.tick(FPS)
@@ -102,11 +103,14 @@ class Shoot(State):
         self.vel.y = self.y * MISSILE_SPEED * dt
         self.action(self.imagemanager.effects[self.__class__.__name__])
 
-class Explode(State):
+class Explosion(State):
     def __init__(self, effect):
         super().__init__(effect)
         self.done = {"None": None}
         self.finish = False
+
+    def startup(self):
+        pass
 
     def events(self):
         pass
@@ -115,5 +119,5 @@ class Explode(State):
         self.vel = vec(0, 0)
         if not self.finish:
             self.action(self.imagemanager.effects[self.__class__.__name__])
-        if self.current_frame == len(self.imagemanager.effects[self.__class__.__name__]):
-            pass
+        if self.current_frame == len(self.imagemanager.effects[self.__class__.__name__]) - 1:
+            self.effect.kill()
