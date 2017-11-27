f49ff7324b00eb348a1e0543469bcac15ab435b1
Ricardo <mandujano.ricardo24@gmail.com>
2017-11-26 23:14:22 -0500

    avanzando mapa campamento

---
+++ img/Act_1/afueras1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="afueras1" tilewidth="40" tileheight="40" tilecount="6080" columns="80">
+ <image source="afueras1.png" trans="ff00ff" width="3200" height="3047"/>
+</tileset>
---
+++ img/Act_1/grave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="graveyardfloor1" tilewidth="40" tileheight="40" tilecount="220" columns="20">
+ <image source="graveyardfloor1.png" trans="ff00ff" width="800" height="479"/>
+</tileset>
