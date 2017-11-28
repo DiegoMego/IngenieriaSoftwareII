0ffd411dda1522123a05749420d678dbeff24e06
Ricardo <mandujano.ricardo24@gmail.com>
2017-11-28 00:55:23 -0500

    nuevo banderas para campamento

---
+++ img/Act_1/banderas1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="banderas campamento" tilewidth="40" tileheight="40" tilecount="304" columns="16">
+ <image source="banderas campamento.png" trans="ff00ff" width="640" height="771"/>
+</tileset>
