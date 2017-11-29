27eaaf79146144d10e5d414941beb36b31d1b2e3
Bruna-M <bruna_morillop@hotmail.com>
2017-11-29 00:40:24 -0500

    Mapa palacio v2 agregado

--- assets/img/Maps/Mapa_Palacio/M_Palace.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="M_Palace" tilewidth="40" tileheight="40" tilecount="1683" columns="187">
- <image source="PC Computer - Diablo 2 Diablo 2 Lord of Destruction - Diablos Lair Wall.png" trans="ff00ff" width="7502" height="396"/>
-</tileset>
