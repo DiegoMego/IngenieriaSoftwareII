2c3a3418f88f16a704629af4642b03bbe7a99320
Bruna-M <bruna_morillop@hotmail.com>
2017-11-29 00:41:39 -0500

    Mapa palacio v2 en carpeta a usar

---
+++ assets/img/Maps/Act_1/O_Palace_Throne.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="O_Palace_Throne" tilewidth="40" tileheight="40" tilecount="192" columns="16">
+ <image source="Trono.png" trans="ff00ff" width="641" height="481"/>
+</tileset>
